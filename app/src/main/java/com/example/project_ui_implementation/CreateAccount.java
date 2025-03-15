ff8c0e4d55243895727f6b50d60815bdb6276090
Jose <josemiguelcaceb@gmail.com>
2025-03-15 15:58:02 -0700

    Go back button was added and small creation of a DropDown box to select the genre (It is not finished just visuals)

--- app/src/main/java/com/example/project_ui_implementation/CreateAccount.java
+++ app/src/main/java/com/example/project_ui_implementation/CreateAccount.java
@@ -1,16 +1,15 @@
 package com.example.project_ui_implementation;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.project_ui_implementation.model.Users;
 import com.google.android.material.textfield.TextInputLayout;
@@ -18,6 +17,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateAccount extends AppCompatActivity {
+    private Button goBackbtn;
+
     FirebaseDatabase database;
     DatabaseReference databaseReference;
     @Override
@@ -26,6 +27,11 @@ public class CreateAccount extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_account);
         database= FirebaseDatabase.getInstance("https://seng-3210-project-4dd9d-default-rtdb.firebaseio.com/");
+        goBackbtn=findViewById(R.id.btnGoBack);
+        goBackbtn.setOnClickListener(v -> {
+            Intent GoBack=new Intent(CreateAccount.this, MainActivity.class);
+            startActivity(GoBack);
+        });
     }
     //Method to add Usernames inside the database.
     public void addUsername(View view){
