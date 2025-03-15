6dc298819cbbf2a3637a7e06080bb20c17628cf6
Brock Young <t00708314@mytru.ca>
2025-03-15 16:58:15 -0700

    shell of authentication. nothing working. stuck on sha key generation

--- app/src/main/java/com/example/project_ui_implementation/MainActivity.java
+++ app/src/main/java/com/example/project_ui_implementation/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,7 +17,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project_ui_implementation.model.Users;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -25,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference databaseReference;
     private TextView googleLogin;
 
+    /** NEW **/
     private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    /** **/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +47,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         database= FirebaseDatabase.getInstance("https://seng-3210-project-4dd9d-default-rtdb.firebaseio.com/");
 
+        /** NEW **/
         mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)) // Request ID token from Google
+                .requestEmail() // Request user's email
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        /** **/
 
         /**
          * Setting specific text, Google Account to be clickable.
@@ -73,6 +96,50 @@ public class MainActivity extends AppCompatActivity {
         googleLogin.setText(spannabletxtGL);
         //----------------------------------------------------------------------//
     }
+
+    /** NEW **/
+    private static final int RC_SIGN_IN = 9001;
+
+    public void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    /** **/
+
+
+    /** NEW **/
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken()); // Authenticate with Firebase
+            } catch (ApiException e) {
+                Log.w("SignInActivity", "Google sign in failed", e);
+            }
+        }
+    }
+
+    /** **/
+
+    /** NEW **/
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Log.d("SignInActivity", "Sign in successful: " + user.getDisplayName());
+                    } else {
+                        Log.w("SignInActivity", "Sign in failed", task.getException());
+                    }
+                });
+    }
+    /** **/
+
     //Method to add Usernames inside the database.
     public void addUsername(View view){
         //Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
